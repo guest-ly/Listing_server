b91e8fecd4818134c29dbb18365c6c48bda4e2d6
yi sun <yisun@yis-MacBook-Pro.local>
2019-07-10 11:11:43 -0700

    pass root path test

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,10 @@
 const request = require('supertest');
 const app = require('../server/index');
 
-describe('Test path',()=>{
-    test('it should response to GET method to /desc', async (done)=>{
-        var num = Math.ceil(Math.random()*100)
+describe('Test the root path',()=>{
+    test('it should response to GET method to /desc',  (done)=>{
         request(app)
-        .get('/listing/desc/'+num)
+        .get('/listing/desc/1')
         .set('Accept','application/json')
         .expect('Content-type',/json/)
         .expect(200)
@@ -14,10 +13,9 @@ describe('Test path',()=>{
             done();
         })
     });
-    test('it should response to GET method to /amenity', async (done)=>{
-        var num = Math.ceil(Math.random()*100)
+    test('it should response to GET method to /amenity',  (done)=>{
         request(app)
-        .get('/listing/amenity/'+num)
+        .get('/listing/amenity/1')
         .set('Accept','application/json')
         .expect('Content-type',/json/)
         .expect(200)
